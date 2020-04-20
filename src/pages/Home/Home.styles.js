9b679ad6e494dccf76dfb3a7ca41e2af3b5a65e4
Anthony Langford <anthony.langford@gmail.com>
2020-04-19 20:51:53 -0400

    refactor-home-page
    Co-authored-by: TylerZhang00 <yiqizhang@gmail.com>

---
+++ src/pages/Home/Home.styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const SentimentInput = styled.input`
+  min-width: 15rem;
+  min-height: 3rem;
+  border: none;
+  background: transparent;
+  border-bottom: 2px solid white;
+  text-align: center;
+  font-size: large;
+  outline: none;
+`;
+
