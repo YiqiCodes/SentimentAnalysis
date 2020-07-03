ad4a0ca10db55d1862c5e95d6a3a3808d4854760
Yiqi Zhang <>
2020-07-02 23:36:51 -0400

    responsiveness fixed

--- src/components/NavBar/NavBar.styles.js
+++ src/components/NavBar/NavBar.styles.js
@@ -3,9 +3,9 @@ import { Button } from "antd";
 
 export const Wrapper = styled.div`
   width: 100%;
+  height: 20%;
   background: #4267b2;
   display: flex;
-  position: fixed;
   top: 0;
   align-items: center;
   justify-content: space-between;
